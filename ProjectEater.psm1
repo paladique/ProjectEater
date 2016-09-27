6aef1195134a570babfd88c7dcd593683eabeca3
Jasmine Greenaway <jasminegweb@gmail.com>
2016-09-27 15:22:23 -0400

    put the function the module

--- ProjectEater.psm1
+++ ProjectEater.psm1
@@ -3,7 +3,70 @@
 #>
 function RemoveOldProjects {
 
-	RemoveVSProjectss
+	[CmdletBinding()]
+	Param(
+	[Parameter(Mandatory=$False,Position=1)]
+	[string]$path
+	)
+
+	RemoveVSProjects $path
 
 		
 }
+
+function RemoveVSProjects {
+	[CmdletBinding()]
+	Param(
+		[Parameter(Mandatory=$False,Position=1)]
+		[string]$path
+	   )
+
+
+	$items = Get-ChildItem -Path $path
+
+	[System.Collections.ArrayList]$OldProjects = New-Object System.Collections.ArrayList
+
+
+	foreach ($item in $items)
+	{
+		  if ($item.Attributes -eq "Directory")
+		  {
+				$end = Get-Date
+				$span = NEW-TIMESPAN –Start $item.LastAccessTime –End $end
+
+				if($span.Days -ge 14 -and $item.GetFiles("*.sln").count -gt 0)
+				{
+					[void]$OldProjects.add($item)	
+				}
+		  }
+	}
+
+	if($OldProjects.Count -gt 0) {
+		"These are over 14 days old, which would you like to delete?"
+	}
+	else {
+		"You don't have any projects to remove, congrats!"
+		return 
+	}
+
+
+
+	foreach ($item in $OldProjects)
+	{
+		Write-Host  $OldProjects.IndexOf($item):  $item.Name  
+	}
+
+	"Press any key to continue, separate mutliple projects with commas."
+	$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+	$toDelete = Read-Host
+
+	if($toDelete.Length -gt 0)
+	{
+		foreach ($item in $toDelete.Split(","))
+		{
+		   Remove-Item $OldProjects.Item($item) -Recurse -Force 
+		}
+	}
+}
+
